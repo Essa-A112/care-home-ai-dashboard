28e75224bc890cff5eecfe1ee4e86a03c36f68a3
Essa-A112 <essaabikar12@gmail.com>
2025-07-18 22:22:56 +0100

    Update dashboard_app.py

--- dashboard_app.py
+++ dashboard_app.py
@@ -95,7 +95,7 @@ if selected_lad:
         shap_path = os.path.join(SHAP_FOLDER, f"{norm_name}.png")
         if os.path.exists(shap_path):
             st.subheader("🧠 SHAP Visualisation")
-            st.image(shap_path, use_container_width=True)
+            st.image(shap_path, use_column_width=True)
         else:
             st.warning("No SHAP image available for this LAD.")
 
