141a4e73d5c1c83e509c140b4230dfeb2e1a074c
Essa-A112 <essaabikar12@gmail.com>
2025-07-18 19:49:22 +0100

    Update dashboard_app.py

--- dashboard_app.py
+++ dashboard_app.py
@@ -7,8 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1kEN9bKk1kHKfBnrRvvUxx6pFPNmatlfH
 """
 
-!pip install streamlit pandas
-
 import streamlit as st
 import pandas as pd
 import os
