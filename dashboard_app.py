30efe9b32881fe92d60cd6acb0c83d6cbfee0266
Essa-A112 <essaabikar12@gmail.com>
2025-07-30 21:49:27 +0100

    Update dashboard_app.py

--- dashboard_app.py
+++ dashboard_app.py
@@ -172,8 +172,7 @@ folium.GeoJson(
 
 map_output = st_folium(m, height=500, returned_objects=["last_active_drawing"])
 
-# Fixed spacing to reduce excess white gap between map and dropdown
-st.markdown("<div style='margin-bottom: -30px;'></div>", unsafe_allow_html=True)
+st.markdown("<div style='height: 30px;'></div>", unsafe_allow_html=True)
 
 
 # === Update session state if map was clicked ===
