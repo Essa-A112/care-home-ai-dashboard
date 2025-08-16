7f60e7af1abf485cfa30654097922269e2ee698f
Essa-A112 <essaabikar12@gmail.com>
2025-08-16 22:21:03 +0100

    Update dashboard_app.py

--- dashboard_app.py
+++ dashboard_app.py
@@ -672,9 +672,7 @@ with tab_map:
         st.markdown("##### SHAP visual")
         shap_path = os.path.join(SHAP_FOLDER, f"{key}.png")
         st.image(shap_path, width=800) 
-        if os.path.exists(shap_path):
-            else:
-                st.info("No SHAP image is available for this LAD.")
+        if os.path.exists(shap_path): else:st.info("No SHAP image is available for this LAD.")
 
 
         st.markdown("##### LLM summary")
